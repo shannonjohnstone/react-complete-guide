6937bae72fa1885ffc7bd5da805d7346a8ed8741
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-08-28 15:56:41 +1000

    add in ingredients transforming logic

--- burger-basic--01-project/src/components/Burger/Burger.js
+++ burger-basic--01-project/src/components/Burger/Burger.js
@@ -2,12 +2,29 @@ import React from 'react'
 import BurgerIngredients from './BurgerIngredients';
 import classes from './Burger.module.css';
 
+/**
+ * 
+ * @param {Object} ingredientsObject 
+ * transform object of ingredients into a burger
+ * iterate over object and check each items value
+ * create an array with the specified amount of undefined items
+ * this is than used to create each item to the specified amount
+ */
+const transformIngredients = (ingredientsObject) => {
+    return Object.keys(ingredientsObject).map(key => {
+        // [...Array(ingredientsObject[key])], is equal to
+        // [...Array(2)], this create am array with that many spots 
+        // in this case those spots will be undefined
+        return [...Array(ingredientsObject[key])].map((_, i) => (
+            <BurgerIngredients key={`${key + i}`} type={key} />
+        ));
+    });
+}
+
 const Burger = (props) => (
     <div className={classes.Burger}>
         <BurgerIngredients type={'bread-top'} />
-        <BurgerIngredients type={'cheese'} />
-        <BurgerIngredients type={'bacon'} />
-        <BurgerIngredients type={'meat'} />
+        {transformIngredients(props.ingredients)}
         <BurgerIngredients type={'bread-bottom'} />
     </div>
 )
--- burger-basic--01-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-basic--01-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 2
+        }
+    }
     render() {
         return (
             <>
                 <p>Burger Builder</p>
-                <Burger  />
+                <Burger ingredients={this.state.ingredients} />
                 <p>Controls</p>
             </>
         )
