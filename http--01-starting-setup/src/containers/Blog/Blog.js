631f79d437cd1143a50810a23e81f9d60b597cb0
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-09-15 12:56:29 +1000

    http request, CSS implement http request for mock post data update flex related css so post a more aligned

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,24 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+        // limit posts set to store and update with author
+        const posts = res.data.slice(0, 4).map(post => ({ ...post, author: 'Dustin' }))
+
+        this.setState({ posts })
+    }
+    render() {
+        console.log(this.state.posts)
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {this.state.posts.map(post => <Post key={post.id} {...post} />)}
                 </section>
                 <section>
                     <FullPost />
