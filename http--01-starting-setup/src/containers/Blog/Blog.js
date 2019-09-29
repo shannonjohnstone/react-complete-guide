f4539abc68487945a8575ae38d7696803a2da032
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-09-29 16:41:29 +1000

    implement the use of the Switch component to only render the route it finds

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import PostContainer from '../Post/PostContainer';
 import NewPostContainer from '../../containers/Post/NewPostContainer';
@@ -9,9 +9,11 @@ import './Blog.css';
 
 const Blog = () => (
     <div className={"Blog"}>
-        <Route path='/' exact component={PreviewPostsContainer} />
-        <Route path="/new-post" component={NewPostContainer} />
-        <Route path="/:id" component={PostContainer} />
+        <Switch>
+            <Route path='/' exact component={PreviewPostsContainer} />
+            <Route path="/new-post" component={NewPostContainer} />
+            <Route path="/:id" component={PostContainer} />
+        </Switch>
     </div>
 );
 
